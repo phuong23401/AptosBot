74360fdb38a645b847b2013713d16a5f5f20db72
phuong23401 <hoaphuong23401@gmail.com>
2024-04-27 10:01:45 +0700

    update code

--- index.ts
+++ index.ts
@@ -2,3 +2,7 @@ import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 
 const aptosConfig = new AptosConfig({ network: Network.TESTNET });
 const aptos = new Aptos(aptosConfig);
+
+const ledgerInfo = await aptos.getLedgerInfo();
+const modules = await aptos.getAccountModules({ accountAddress: "0x123" });
+const tokens = await aptos.getAccountOwnedTokens({ accountAddress: "0x123" });
