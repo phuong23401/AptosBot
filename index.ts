78f96c36eb9b297c1395846eb4bbe7e6ba4ca77a
phuong23401 <hoaphuong23401@gmail.com>
2024-04-28 12:16:49 +0700

    update code

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
-import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+import { Aptos, AptosConfig, Network } from '@aptos-labs/ts-sdk'
 
-const aptosConfig = new AptosConfig({ network: Network.TESTNET });
-const aptos = new Aptos(aptosConfig);
+const aptosConfig = new AptosConfig({ network: Network.TESTNET })
+const aptos = new Aptos(aptosConfig)
 
-const ledgerInfo = await aptos.getLedgerInfo();
-const modules = await aptos.getAccountModules({ accountAddress: "0x123" });
-const tokens = await aptos.getAccountOwnedTokens({ accountAddress: "0x123" });
+const ledgerInfo = await aptos.getLedgerInfo()
+const modules = await aptos.getAccountModules({ accountAddress: '0x123' })
+const tokens = await aptos.getAccountOwnedTokens({ accountAddress: '0x123' })
+
+console.log(ledgerInfo)
+console.log(modules)
+console.log(tokens)
