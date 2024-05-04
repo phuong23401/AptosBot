9da224ce95defe7fbe4fe05d31fa62b8fefd5f2c
phuong23401 <hoaphuong23401@gmail.com>
2024-05-05 04:07:03 +0700

    update code

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ const transaction = await aptos.transaction.build.simple({
   },
 })
 
-console.log(ledgerInfo)
 console.log(modules)
+console.log(ledgerInfo)
 console.log(tokens)
 console.log(transaction)
